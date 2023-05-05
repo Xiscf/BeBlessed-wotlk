a9ca9b0d80eaba8852bfcbbb4d5dc010608ef238
Xiscf <Xiscf@users.noreply.github.com>
2023-05-05 22:17:40 +0200

    Update core.lua

--- core.lua
+++ core.lua
@@ -21,7 +21,7 @@ BeBlessed.BlessingIDs = {
 -- Do not take in consideration the minor glyphe that increase the buff's duration
 -- on yourself from yourself. 
 local BlessingDurations = {
-    ["Blessing of Might"] =  600,     -- 300 (300 = 5min, 600 = 10min, 900 = 15min, 1800 = 30m)
+    ["Blessing of Might"] =  600,    -- 300 (300 = 5min, 600 = 10min, 900 = 15min, 1800 = 30m)
     ["Blessing of Wisdom"] = 600,    -- 300 (300 = 5min, 600 = 10min, 900 = 15min, 1800 = 30m)
     ["Blessing of Kings"] = 600,     -- 300 (300 = 5min, 600 = 10min, 900 = 15min, 1800 = 30m)
     ["Blessing of Sanctuary"] = 600, -- 300 (300 = 5min, 600 = 10min, 900 = 15min, 1800 = 30m)
@@ -32,108 +32,107 @@ local BlessingDurations = {
 }
 
 local function EqualAny(val, target)
-    for v,_ in pairs(target) do
-        if val == v then
-            return true;
-        end
-    end
-    return false;
+   for v,_ in pairs(target) do
+      if val == v then
+         return true;
+      end
+   end
+   return false;
 end
 
 
 function eventHandler.events:ADDON_LOADED(event, addon)
-    if (addon == "BeBlessed") then
+   if (addon == "BeBlessed") then
 
-        local _, class = UnitClass("player");
-        if (class ~= "PALADIN") then
-            return;
-        end
+      local _, class = UnitClass("player");
+      if (class ~= "PALADIN") then
+         return;
+      end
 
-        if type(BeBlessedData) ~= "table" then
-            BeBlessedData = {}
-        end
+      if type(BeBlessedData) ~= "table" then
+         BeBlessedData = {}
+      end
 
-        if (type(BeBlessedData[INDEX]) ~= "table") then
-            BeBlessedData[INDEX] = {};
-            BeBlessedData[INDEX].settings = {}
-            BeBlessedData[INDEX].settings.modes = {[0] = 0, [1] = 0, [2] = 0, [3] = 0, [4] = 0, [5] = 0, [6] = 0, [7] = 0};
-            BeBlessedData[INDEX].buffStatus = {};
-            BeBlessedData[INDEX].settings.selectedBlessings = {[0] = 0, [1] = 0, [2] = 0, [3] = 0, [4] = 0, [5] = 0, [6] = 0, [7] = 0};
-        end
+      if (type(BeBlessedData[INDEX]) ~= "table") then
+         BeBlessedData[INDEX] = {};
+         BeBlessedData[INDEX].settings = {}
+         BeBlessedData[INDEX].settings.modes = {[0] = 0, [1] = 0, [2] = 0, [3] = 0, [4] = 0, [5] = 0, [6] = 0, [7] = 0};
+         BeBlessedData[INDEX].buffStatus = {};
+         BeBlessedData[INDEX].settings.selectedBlessings = {[0] = 0, [1] = 0, [2] = 0, [3] = 0, [4] = 0, [5] = 0, [6] = 0, [7] = 0};
+      end
 
-        BeBlessed:CreateSpellNameLocale();
-        BeBlessed:CreateBuffFrame();
+      BeBlessed:CreateSpellNameLocale();
+      BeBlessed:CreateBuffFrame();
 
-        print("Be|cffF58CBABlessed|r: Loaded");
-    end
+      print("Be|cffF58CBABlessed|r: Loaded");
+   end
 end
 
 function eventHandler.events:PLAYER_REGEN_ENABLED(event,...)
-    inCombat = false;
+   inCombat = false;
 end
 
 function eventHandler.events:PLAYER_REGEN_DISABLED(event,...)
-    inCombat = true;
+   inCombat = true;
 end
 
 function eventHandler.events:COMBAT_LOG_EVENT_UNFILTERED(event)
+   local _, eventType, _, sourceGUID, sourceName, spellID, _, destGUID, destName, _, _, _, spellName = CombatLogGetCurrentEventInfo();
 
-    local _, eventType, _, sourceGUID, sourceName, spellID, _, destGUID, destName, _, _, _, spellName = CombatLogGetCurrentEventInfo();
-
-    if (sourceGUID == UnitGUID("player") and eventType == "SPELL_CAST_SUCCESS") then
-
-        local engSpellName = BeBlessed.toEnglish[spellName];
-
-        if EqualAny(engSpellName, BlessingDurations) then
-            BeBlessedData[INDEX].buffStatus[destGUID] = {applied=GetTime(), spellName=engSpellName, spellID=spellID, duration=BlessingDurations[engSpellName]};
-        end
+   if (sourceGUID == UnitGUID("player") and eventType == "SPELL_CAST_SUCCESS") then
+      local engSpellName = BeBlessed.toEnglish[spellName];
+      
+      if EqualAny(engSpellName, BlessingDurations) then
+         BeBlessedData[INDEX].buffStatus[destGUID] = {applied=GetTime(), spellName=engSpellName, spellID=spellID, duration=BlessingDurations[engSpellName]};
+      end
         
-    end
+   end
 end
 
 for event,_ in pairs(eventHandler.events) do
-    eventHandler:RegisterEvent(event);
+   eventHandler:RegisterEvent(event);
 end
+
 eventHandler:SetScript("OnEvent", function(self,event,...)
-    eventHandler.events[event](self, event, ...);
+   eventHandler.events[event](self, event, ...);
 end)
 
 
 function BeBlessed:GetGUIDStatus(guid)
-    if (type(BeBlessedData[INDEX].buffStatus[guid]) == "table") then
-        return BeBlessedData[INDEX].buffStatus[guid]
-    end
-    return nil;
+   if (type(BeBlessedData[INDEX].buffStatus[guid]) == "table") then
+      return BeBlessedData[INDEX].buffStatus[guid]
+   end
+   return nil;
 end
 
 function BeBlessed:UnitHasBuff(unitID, buffName)
-    for i = 1,40 do
-        local name = UnitBuff(unitID, i);
-        if name == buffName then
-            return true;
-        end
-    end
-    return false;
+   for i = 1,40 do
+      local name = UnitBuff(unitID, i);
+      if name == buffName then
+         return true;
+      end
+   end
+   return false;
 end
 
 function BeBlessed:InCombat()
-    return inCombat;
+   return inCombat;
 end
 
 function BeBlessed:GetAvailableSpells()
-    local avaialble = {};
-    for k,v in pairs(BeBlessed.BlessingIDs) do 
-        if IsSpellKnown(v) then
-            avaialble[k] = true;
-        end
-    end
-    return avaialble;
+   local avaialble = {};
+   for k,v in pairs(BeBlessed.BlessingIDs) do 
+      if IsSpellKnown(v) then
+         avaialble[k] = true;
+      end
+   end
+   return avaialble;
 end
 
 function BeBlessed:CreateSpellNameLocale()
-    for k,v in pairs(BeBlessed.BlessingIDs) do
-        local name = GetSpellInfo(v);
-        BeBlessed.toLocal[k] = name;
-        BeBlessed.toEnglish[name] = k;
-    end
+   for k,v in pairs(BeBlessed.BlessingIDs) do
+      local name = GetSpellInfo(v);
+      BeBlessed.toLocal[k] = name;
+      BeBlessed.toEnglish[name] = k;
+   end
 end
